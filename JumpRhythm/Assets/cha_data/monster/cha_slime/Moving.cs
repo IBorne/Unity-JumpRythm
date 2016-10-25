05f5977101f467482a0b639c3c78d03af71eb6e7
turliem <marie.turlier@epita.fr>
2016-10-25 14:51:12 +0200

    plus d'araignée mais un slime qui fonctionne

---
+++ JumpRhythm/Assets/cha_data/monster/cha_slime/Moving.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour {
+    private Animator anim;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+            anim.SetBool("Walk", true);
+            float translation = Time.deltaTime * 2;
+            transform.Translate(-translation, 0, 0);
+    }
+}
